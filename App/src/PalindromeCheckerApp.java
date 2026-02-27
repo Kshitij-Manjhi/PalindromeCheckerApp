91179fdcb2017ec3ec6d6db6c6d6cc8b70d2e932
Kshitij-Manjhi <kshitijmanjhi@gmail.com>
2026-02-27 16:42:11 +0530

    UC3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         /**
-         * Application entry point for UC2
+         * Application entry point for UC3
          *
          * This is a Palindrome Checker App Welcome Message
          *
          * @author Developer
-         * @version 2.0
+         * @version 3.0
         */
         String input = "madam";
+        String reversed = "";
         boolean isPalindrome = false;
         System.out.println("Input text: " + input);
-        for (int i = 0; i < input.length() / 2; i++){
-            if(input.charAt(i)!= input.charAt(input.length() - 1 - i)){
-                isPalindrome = false;
-                break;
-            }
+        for (int i = input.length() - 1; i >= 0 ; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+        if (input.equals(reversed)){
+            isPalindrome = true;
         }
         System.out.println("Is it Palindrome? : "+ isPalindrome);
     }
